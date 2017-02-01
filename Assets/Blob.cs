e95cc3f0b727bd3addf2093d593ab4afc7083960
Unknown <teaguefcs@gmail.com>
2017-02-01 18:11:28 -0500

    Added Player, Walls, Floors and Jumping

---
+++ Assets/Blob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blob : MonoBehaviour {
+
+    Rigidbody2D blob;
+    bool isOnGround = true;
+
+    // Use this for initialization
+    void Start() {
+        blob = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey("a"))
+        {
+            blob.AddForce(new Vector2(-1, 0), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey("d"))
+        {
+            blob.AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown("w") && isOnGround)
+        {
+            blob.AddForce(new Vector2(0, 32), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Floor")
+        {
+            isOnGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Floor")
+        {
+            isOnGround = false;
+        }
+    }
+}
